47ca3f3266c2376fedc8aa4ebc1e4aabea806669
daniil39kim <164365943+daniil39kim@users.noreply.github.com>
2024-10-22 10:11:01 +0500

    build(core): add custom dependency configuration for core plugins and their automatic bundling.

--- opus-core/build.gradle.kts
+++ opus-core/build.gradle.kts
@@ -11,6 +11,29 @@ repositories {
     mavenCentral()
 }
 
+// Custom configuration to mark core plugins
+val opusPlugin by configurations.creating {
+    isCanBeConsumed = false
+    isCanBeResolved = true
+}
+
+tasks.register("copyOpusPlugins"){
+    val opusPlugins = opusPlugin.dependencies.filterIsInstance<ProjectDependency>().map { it.dependencyProject }
+    inputs.files(opusPlugin.files) // Run only if files changed
+    println(opusPlugin.files)
+    outputs.dir(layout.buildDirectory.dir("libs/plugins"))
+    for( pluginProject in opusPlugins ){
+        val outputDir = layout.buildDirectory.dir("libs/plugins/${pluginProject.name}")
+        doLast{
+            copy{
+                println("Copying ${pluginProject.name}")
+                from(pluginProject.layout.buildDirectory.dir("libs"))
+                into(outputDir)
+            }
+        }
+    }
+}
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
@@ -23,6 +46,18 @@ dependencies {
 
     implementation("info.picocli:picocli:4.7.6")
     annotationProcessor("info.picocli:picocli-codegen:4.7.6")
+
+    opusPlugin(project(":opus-core-plugins:opus-core-plugins-magnum"))
+    opusPlugin(project(":opus-core-plugins:opus-core-plugins-maestro"))
+}
+
+tasks.register("rebuildAndCopyOpusPlugins"){
+    dependsOn(opusPlugin)
+    finalizedBy("copyOpusPlugins")
+}
+
+tasks.named("jar") {
+    finalizedBy("rebuildAndCopyOpusPlugins")
 }
 
 tasks.test {
