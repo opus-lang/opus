5233d0a8d8a262f0478dc1635f6b3188872a7581
daniil39kim <164365943+daniil39kim@users.noreply.github.com>
2024-10-27 17:19:13 +0500

    fix(core-plugins-magnum): fix a bug, where due to name mangling, context entries were inaccessible.

--- opus-core-plugins/opus-core-plugins-magnum/src/main/java/dev/opuslang/opus/core/plugins/magnum/api/pipeline/PassContext.java
+++ opus-core-plugins/opus-core-plugins-magnum/src/main/java/dev/opuslang/opus/core/plugins/magnum/api/pipeline/PassContext.java
@@ -7,24 +7,17 @@ public class PassContext {
 
     private final Map<String, Object> data = new HashMap<>();
 
-    private static String mangleKey(String key){
-        return "$" + key + "$";
-    }
-
     public void put(String key, Object value){
-        key = PassContext.mangleKey(key);
         if(this.data.containsKey(key)) throw new IllegalArgumentException("Key already exists.");
         this.data.put(key, value);
     }
 
     public Object rawGet(String key){
-        key = PassContext.mangleKey(key);
         if(!this.data.containsKey(key)) throw new IllegalArgumentException("Invalid key.");
         return this.data.get(key);
     }
 
     public <T> T get(String key, Class<T> type){
-        key = PassContext.mangleKey(key);
         Object value = this.rawGet(key);
         if(!type.isInstance(value)) throw new IllegalArgumentException("Invalid type.");
         return type.cast(value);
