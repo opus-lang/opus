2c48b1e092ee658170c968f7205eed3a701df380
daniil39kim <164365943+daniil39kim@users.noreply.github.com>
2024-11-19 19:38:19 +0500

    chore(misc): loose ends.

--- opus-api/src/main/java/dev/opuslang/opus/api/plugin/annotation/Plugin.java
+++ opus-api/src/main/java/dev/opuslang/opus/api/plugin/annotation/Plugin.java
@@ -6,6 +6,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+// TODO: Scope inside "OpusAPI" class.
 @Retention(RetentionPolicy.CLASS)
 @Target(ElementType.MODULE)
 public @interface Plugin {
